2f596111d14dd5be2e3dcaba151407ccb17402d4
Mohamed-Elzohary <MohamedElzohary2001@outlook.com>
2022-10-27 05:38:58 +0200

    updated: same design pattern as express-generator

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
+var express = require('express');
+var router = express.Router();
 
-const controller = require(path.resolve(__basename, 'controllers', 'app.controllers'))
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-router.get('/', controller.getLanding)
-
-module.exports = router
+module.exports = router;
